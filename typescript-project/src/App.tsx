880d3d8350c939ed782a277dc1790a7f78f3a4a2
Natalia-dot <nataliadurantegarcia@gmail.com>
2024-01-19 23:58:05 +0100

    routing implemented and working

--- typescript-project/src/App.tsx
+++ typescript-project/src/App.tsx
@@ -1,10 +1,12 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, RouterProvider } from 'react-router-dom';
 import './App.css';
 import {Footer, Nav} from './components/index'
+import { router } from './routes/routes';
 
 const App: React.FC = () => {
   return (
    <>
+
   <Nav theme='dark'/>
   <Outlet/>
   <Footer theme='dark'/>
