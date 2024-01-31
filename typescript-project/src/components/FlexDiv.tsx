6d92922e65eb98d71bfd49099c70347a26d4dbe3
Natalia-dot <nataliadurantegarcia@gmail.com>
2024-01-31 14:17:53 +0100

    div container styled

--- typescript-project/src/components/FlexDiv.tsx
+++ typescript-project/src/components/FlexDiv.tsx
@@ -6,9 +6,10 @@ const StyledFlexDiv = styled.div<FlexDivProps>`
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    width: ${(props) => props.w ? props.w : '90vw'};
-    height: ${(props) => props.h ? props.h : '30vh'};
+    width: ${(props) => props.$w ? props.$w : '90vw'};
+    height: ${(props) => props.$h ? props.$h : '30vh'};
     background: rgba(0, 0, 0, 30%);
+    margin: 3rem;
 `
 
 export const FlexDiv = (props: FlexDivProps) => {
